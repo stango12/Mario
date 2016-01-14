00d76908dd695e90f8ac972656ea3bcdc8fedb6c
Steven <stevenyztang@gmail.com>
2016-01-14 00:24:45 -0600

    first commit

---
+++ html/src/com/stango/mario/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.stango.mario.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.stango.mario.Mario;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Mario();
+        }
+}
